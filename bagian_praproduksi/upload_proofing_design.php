3fbdd6513252735082baf74a5aa2ee015e7a09fa
Rivalda <rivaldarafalrachman2016@gmail.com>
2024-08-25 15:18:18 +0700

    update

--- bagian_praproduksi/upload_proofing_design.php
+++ bagian_praproduksi/upload_proofing_design.php
@@ -184,59 +184,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['proofing_file'])) {
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Produk
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="manage_products.php">List Produk</a></li>
-                            <li><a class="dropdown-item" href="add_product.php">Tambah Produk</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Manajemen Stok
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="manage_inventory.php">Stok Bahan</a></li>
-                            <li><a class="dropdown-item" href="add_inventory.php">Tambah Bahan Baku</a></li>
-                            <li><a class="dropdown-item" href="request_stock.php">Permintaan Bahan Baku</a></li>
-                            <li><a class="dropdown-item" href="manage_requests.php">Cetak</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Pengeluaran
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="input_expense.php">Tambah Pengeluaran</a></li>
-                            <li><a class="dropdown-item" href="manage_expenses.php">Laporan Pengeluaran</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Pesanan Konsumen
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="manage_orders.php">Konsumen Perorangan</a></li>
                             <li><a class="dropdown-item" href="manage_company_orders.php">Konsumen Perusahaan</a></li>
                         </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="manage_returns.php">Pengembalian</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="company_register.php">Daftar Akun Perusahaan</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Kelola Akun Konsumen
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="manage_accounts.php">Akun Perorangan</a></li>
-                            <li><a class="dropdown-item" href="manage_company_accounts.php">Akun Perusahaan</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="manage_users.php">Kelola Semua Akun</a>
                     </li>
                 </ul>
             </div>
